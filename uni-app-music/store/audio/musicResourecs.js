d3bca79eff377e3a55e421a287389d13206c1dfd
CHINAHUANGQIJIA <1559022933qq.com>
2020-11-12 15:28:01 +0800

    项目初始化

---
+++ uni-app-music/store/audio/musicResourecs.js
@@ -0,0 +1,20 @@
+export default {
+	musicResourecs: [{
+		id:20001,
+		name:"huoyan",
+		src:'/static/mp3/huoyan.mp3',
+		singer:{
+			name:"女王蜂",
+			synopsis:"女王蜂（日语：じょおうばち）是日本的四人摇滚乐团，2009年在兵库县神户市组成。2011年正式出道，有“日本最神秘乐团”、“眼见不一定为凭乐团”之称"
+		}
+	},
+	{
+		id:20002,
+		name:"huoyanop",
+		src:'/static/mp3/huoyan-op.mp3',
+		singer:{
+			name:"女王蜂",
+			synopsis:"女王蜂（日语：じょおうばち）是日本的四人摇滚乐团，2009年在兵库县神户市组成。2011年正式出道，有“日本最神秘乐团”、“眼见不一定为凭乐团”之称"
+		}
+	}]
+}
